d4e69458e1f2a903132b54adb7daf968ea52eecc
Mohamad Ojail <mohamad.ojail@gmail.com>
2021-12-29 12:28:49 +0100

    kommenterat den nya klassen

--- src/com/yajava/berakning/StartNummerLotto.java
+++ src/com/yajava/berakning/StartNummerLotto.java
@@ -11,15 +11,18 @@ public class StartNummerLotto {
 	public static void geStartNummer(List<Akare> akarListan) {
 				
 		Random rnd = new Random();
+		// skapa en temp lista med slumpade startnummer
 		List<Integer> startNumbers = new ArrayList<>();
 		akarListan.forEach(x->startNumbers.add(rnd.nextInt(99) + 1));
 		
 		boolean isUnique = true;
 		
+		// använd metoden isUnique för att kolla dubbla värden
 		for( Integer x : startNumbers) {
 			if(!isUnique(startNumbers, x)) isUnique = false;
 		}
 		
+		// om ett startnummer är inte unikt slumpa det igen och sedan kolla igen
 		while (!isUnique) {
 			for( Integer x : startNumbers) {
 				if(!isUnique(startNumbers, x)) {
@@ -31,11 +34,13 @@ public class StartNummerLotto {
 			}
 		}
 		
+		// efter att templistan är färdig tilldela startnummerna till åkarlistas objekten
 		for ( Akare akare : akarListan) {
 			akare.setStartNr(startNumbers.get(  akarListan.indexOf(akare)  ) );
 		}
 	}
 	
+	// ett startnummer ska finnas bara en gång i listan
 	private static boolean isUnique(List<Integer> list, int x) {
 		
 		long count = list.stream()
